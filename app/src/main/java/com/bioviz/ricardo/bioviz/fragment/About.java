ff0522d1abab9776e51df9414a35a0e4089f2dec
ricardo <ricardo.amorim3@gmail.com>
2015-02-05 15:16:18 +0000

    Initial commit. Base project created, including necessary libraries: Gson, Support, Volley. Created initial About fragment

---
+++ app/src/main/java/com/bioviz/ricardo/bioviz/fragment/About.java
@@ -0,0 +1,56 @@
+package com.bioviz.ricardo.bioviz.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bioviz.ricardo.bioviz.R;
+import com.bioviz.ricardo.bioviz.activity.Home;
+import com.bioviz.ricardo.bioviz.utils.Values;
+
+/**
+ * Fragment to show additional info about the author and the involved technologies
+ */
+public class About extends Fragment {
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment About.
+     */
+    public static About newInstance() {
+        About fragment = new About();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public About() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((Home) activity).onSectionAttached(Values.section_about_id);
+    }
+}
---
+++ app/src/main/java/com/bioviz/ricardo/bioviz/utils/Values.java
@@ -0,0 +1,8 @@
+package com.bioviz.ricardo.bioviz.utils;
+
+/**
+ * Created by ricardo on 05-02-2015.
+ */
+public class Values {
+    public static int section_about_id = 3;
+}
