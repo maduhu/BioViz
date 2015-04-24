2eac88717c785d5c5030455f11b10e02203a1e48
Ricardo Amorim <ricardo.amorim3@gmail.com>
2015-04-24 17:15:54 +0100

    Changed home fragmet to expand the switcher when clicking (still have to find a better way to manage the index)

--- app/src/main/java/com/bioviz/ricardo/bioviz/fragment/Home.java
+++ app/src/main/java/com/bioviz/ricardo/bioviz/fragment/Home.java
@@ -1,10 +1,12 @@
 package com.bioviz.ricardo.bioviz.fragment;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,7 @@ import com.bioviz.ricardo.bioviz.activity.MainActivity;
 import com.bioviz.ricardo.bioviz.utils.Values;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,8 +32,8 @@ public class Home extends Fragment implements ViewSwitcher.ViewFactory, View.OnC
     private TextSwitcher switcher;
     private int mIndex = 0;
     private Timer mTimer;
-    ArrayList<String> homeMessages;
-    ArrayList<String> homeDescriptions;
+    private String[] homeMessages;
+    private String[] homeDescriptions;
 
     /**
      * Use this factory method to create a new instance of
@@ -66,26 +69,19 @@ public class Home extends Fragment implements ViewSwitcher.ViewFactory, View.OnC
         switcher.setFactory(this);
 
         Animation in = AnimationUtils.loadAnimation(getActivity(),
-                android.R.anim.slide_out_right);
+                android.R.anim.fade_in);
         Animation out = AnimationUtils.loadAnimation(getActivity(),
-                android.R.anim.slide_in_left);
+                android.R.anim.fade_out);
 
         switcher.setInAnimation(in);
         switcher.setOutAnimation(out);
         switcher.setOnClickListener(this);
-        switcher.setText("Welcome to the BioViz application! BioViz is focused on making it easy to find specific details about different species, allowing you to get in touch with the surrounding biodiversity.");
 
-        homeMessages = new ArrayList<String>() {{
-            add("I will be your guide throughout this application. I would like you to know that my developer is very eager to get your feedback! If you want to know more about him, tap here.");
-            add("BioViz gets all its data from two main biodiversity sources: GBIF and iNaturalist. These are often used by biologists and biodiversity specialists to share data about different observations and sightseeings.");
-            add("You can customize the results shown in this application. This customization can go from requiring observations with images attached, to restricting network data to wifi-based connections.");
-        }};
 
-        homeDescriptions = new ArrayList<String>() {{
-            add("Hey! My name is Ricardo, I am the developer of this application. I am kinda new to this mobile application world so be patient if something goes wrong. I enjoy getting info about species around me and explore different locations' habitats. With that in mind, I started developing this application and focused on searching for biodiversity exchange points to extract such data and show it to you in the application.");
-            add("BioViz is this. iNaturalist is like a wikipedia-bioviz everyone can submit, no one cares about curating resources");
-            add("If you want a more visually-intense experience, you can request that the search results include images of the observation. This is sometimes difficult to happen when getting results from GBIF, but it is still worth the try. iNaturalist is often richer in terms of related media and will certainly get all the results with images. Nevertheless, if a specific observation has no pictures associated with it, it is still possible to get pictures from the associated species by clicking on it.");
-        }};
+        homeMessages = getResources().getStringArray(R.array.home_messages);
+        homeDescriptions = getResources().getStringArray(R.array.home_expanded_messages);
+
+        switcher.setText(homeMessages[mIndex]);
 
         mTimer =new Timer();
         mTimer.scheduleAtFixedRate(new TimerTask() {
@@ -129,8 +125,14 @@ public class Home extends Fragment implements ViewSwitcher.ViewFactory, View.OnC
 
     @Override
     public void onClick(View view) {
+
+        ObjectAnimator animation = ObjectAnimator.ofInt(switcher, "maxLines", 40);
+        switcher.setText(homeDescriptions[mIndex]);
+        animation.setDuration(200).start();
+
+        /*
         new AlertDialog.Builder(getActivity())
-                .setMessage(homeDescriptions.get(mIndex))
+                .setMessage(homeDescriptions[mIndex])
                 .setIcon(R.drawable.ic_yok_angry)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
@@ -139,11 +141,13 @@ public class Home extends Fragment implements ViewSwitcher.ViewFactory, View.OnC
                     }
                 })
                 .show();
+                */
     }
 
     private void dispatchNextMessage() {
-        switcher.setText(homeMessages.get(mIndex));
-        if (mIndex == homeMessages.size()-1) {
+        Log.e("INDEX", "" + mIndex);
+        switcher.setText(homeMessages[mIndex]);
+        if (mIndex == homeMessages.length-1) {
             mIndex = 0;
         } else {
             mIndex++;
